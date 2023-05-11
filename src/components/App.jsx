0e126fc183d512b20432db14a104f884043ae6d0
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-05-11 18:28:38 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,33 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+
+    const contacts = localStorage.getItem('contacts');
+    const parsedTodos = JSON.parse(contacts);
+
+    //- === имитация запроса ====
+
+    //   setTimeout(() => {
+    //    this.setState({ todos: parsedTodos });
+    //   }, 2000);
+
+    //- проверка на наличие записей в тудус, если NULL - может все поламается
+
+    if (parsedTodos) {
+      this.setState({ contacts: parsedTodos });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+     console.log('contacts is update');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+
+  }
+
   addContact = contact => {
     this.setState(prevState => ({
       contacts: [contact, ...prevState.contacts],
